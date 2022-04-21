b58103d240d8421e57edfdbbdf04b50d50ab1cc4
«Vova <vrobskiy@gmail.com>
2022-04-21 20:10:14 +0300

    add example 001 for

---
+++ 001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for(int i = 1; i <= number; i++)
+{
+    sum += i;
+}
+Console.WriteLine("Ответ: " + sum);
