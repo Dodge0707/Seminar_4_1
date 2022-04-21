cdde44b52f9ec9f7494f1d7c2816e87b3fb855aa
«Vova <vrobskiy@gmail.com>
2022-04-21 20:54:10 +0300

    add example 002 for !=

---
+++ 002/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int kolvo = number.Length;
+int result = 0;
+
+for (int i = 0; i <= kolvo; i++)
+{
+    result = i;
+}
+Console.WriteLine(result);
+
+/*Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+while(number !=0)
+{
+    number = number/10;
+    index++;
+}
+Console.WriteLine(index);*/
