6396dc8085b5c3a8b5a980a522205d6783dc07b9
«Vova <vrobskiy@gmail.com>
2022-04-21 20:59:52 +0300

    add example 003 result = result * index

---
+++ 003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+for (int index = 1; index <= number; index++)
+{
+    result = result * index;
+}
+Console.WriteLine(result);
